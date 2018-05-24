7467f8d8348d4af89948c7d87e29ed1adb12b116
johannesfahringer <j.fahringer@hotmail.com>
2018-05-24 13:07:57 +0200

    FIRST COMMIT

--- src/main/java/EchoBot.java
+++ src/main/java/EchoBot.java
@@ -1,8 +1,13 @@
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
+import java.io.*;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.regex.Pattern;
 
 public class EchoBot extends TelegramLongPollingBot {
@@ -13,6 +18,12 @@ public class EchoBot extends TelegramLongPollingBot {
                 update.getMessage().hasText()) {
 
             String response = getResponse(update.getMessage().getText());
+
+            try {
+                System.out.println(readJsonFromUrl("https://webproxy.fh-kufstein.ac.at/cafeteria/getcafeteriadata;from=21.04.2018;"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
             if (!response.isEmpty()) {
                 SendMessage message = new SendMessage()
@@ -35,11 +46,48 @@ public class EchoBot extends TelegramLongPollingBot {
         return "";
     }
 
+    private static String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
+
+    public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+        InputStream is = new URL(url).openStream();
+        try {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            String jsonText = readAll(rd);
+            JSONObject json = new JSONObject(jsonText);
+            return json;
+        } finally {
+            is.close();
+        }
+    }
+
+    public static JSONObject readJsonFromUrlItem(String url, JSONObject name) throws IOException, JSONException {
+        InputStream is = new URL(url).openStream();
+        try {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            String jsonText = readAll(rd);
+            JSONObject json = new JSONObject(jsonText);
+            String jsonnew = json.getString("preis");
+            System.out.println(jsonnew);
+            return json;
+        } finally {
+            is.close();
+        }
+    }
+
+
+
     public String getBotUsername() {
-        return "BOT USERNAME EINTRAGEN";
+        return "Cafeteria_FH_bot";
     }
 
     public String getBotToken() {
-        return "BOT TOKEN EINTRAGEN";
+        return "561379899:AAE1ihEja2UH42vGxVmH4jk1xyYT_iu1BFE";
     }
 }
